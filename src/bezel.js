20811c6f87e2cfef44a2f0e54e7d3c3b29f407d9
Dima Tisnek <dimaqq@gmail.com>
2020-07-13 21:23:28 +0900

    first build

---
+++ src/bezel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bezel = () => {
+return "FIXME";
+};
+
+export default Bezel;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Bezel from "bezel";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Bezel/>
+  </React.StrictMode>,
+  document.getElementById("root"));
